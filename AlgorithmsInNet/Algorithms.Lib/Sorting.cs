602249ef107732cd56de749b39c69d8038738222
mydatng <mydatng@gmail.com>
2022-07-04 00:46:52 +0700

    Add project files.

---
+++ AlgorithmsInNet/Algorithms.Lib/Sorting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Lib
+{
+    public class Sorting
+    {
+        private static void BubleSort(int[] array)
+        {
+            for (int i = array.Length - 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+            {
+                return;
+            }
+
+            int temp = array[i];
+
+            array[i] = array[j];
+
+            array[j] = temp;
+        }
+    }
+}
---
+++ Algorithsm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithsm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
